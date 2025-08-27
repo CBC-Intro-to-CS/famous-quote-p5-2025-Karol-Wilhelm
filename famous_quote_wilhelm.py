f0fde378ff5f3fc7972d15dcb144562f3c339769
Karol <WilhelmSanchezJ@cbchs.org>
2025-08-26 21:33:19 -0500

    Completed assignment Famous Quote

---
+++ famous_quote_wilhelm.py
@@ -0,0 +1,5 @@
+print("\"Leadership consists of picking good men and helping them do their best.\"\n\t-Chester Nimitz")
+name = "Chester Nimitz"
+famous_person = name
+messege = "\"Leadership consists of picking good men and helping them do their best.\"\n\t-Chester Nimitz"
+print(messege)
